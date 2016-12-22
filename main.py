566cd99b0fc9065eb281e0517f3d154258613788
François Brault <francois@nauting.com>
2016-12-22 17:45:46 +0100

    rename variables

--- main.py
+++ main.py
@@ -12,50 +12,49 @@ import Queue
 
 
 # Addon Script information
-__addonID__ = provider.ADDON.getAddonInfo('id')
-API_URL = provider.ADDON.getSetting("base_url")
+_ID_ = provider.ADDON.getAddonInfo('id')
+_API_ = provider.ADDON.getSetting("base_url")
 _USERNAME_ = provider.ADDON.getSetting("username")
 _PASSWORD_ = provider.ADDON.getSetting("password")
-titreVF = provider.ADDON.getSetting("titreVF")
-passkey = provider.ADDON.getSetting("passkey")
-icon = xbmcaddon.Addon().getAddonInfo('icon')
-filtre_film = provider.ADDON.getSetting("filtre_f")
-filtre_serie = provider.ADDON.getSetting("filtre_s")
-serie_en_plus = provider.ADDON.getSetting("serie_en_plus")
+_TITLE_VF_ = provider.ADDON.getSetting("titreVF")
+_PASSKEY_ = provider.ADDON.getSetting("passkey")
+_ICON_ = xbmcaddon.Addon().getAddonInfo('icon')
+_FILTER_MOVIE_ = provider.ADDON.getSetting("filtre_f")
+_FILTER_SERIES_ = provider.ADDON.getSetting("filtre_s")
+_FILTER_SERIES_FULL_ = provider.ADDON.getSetting("serie_en_plus")
+_FILTER_LIMIT_ = 15
 
+USER_CREDENTIALS = {}
+USER_CREDENTIALS_FILE = xbmc.translatePath("special://profile/addon_data/%s/token.txt" % _ID_)
 
-limit = 15  # provider.ADDON.getSetting("limit")
-
-USER_CREDENTIALS_FILE = xbmc.translatePath("special://profile/addon_data/%s/token.txt" % __addonID__)
-
-user_credentials = {}
-
-tmdbUrl = 'http://api.themoviedb.org/3'
-tmdbKey = '8d0e4dca86c779f4157fc2c469c372ca'    # mancuniancol's API Key.
+TMDB_URL = 'http://api.themoviedb.org/3'
+TMDB_KEY = '8d0e4dca86c779f4157fc2c469c372ca'    # mancuniancol's API Key.
 
 # Categories ID /categories/tree
 CAT_VIDEO = '210'
 CAT_MOVIE = '631'
-CAT_TV = '433'
 CAT_MOVIE_ANIM = '455'
-CAT_TV_ANIME = '637'
+CAT_SERIES = '433'
+CAT_SERIES_ANIMATED = '637'
+CAT_EMISSION = '639'
 
-new_url = 'https://api.t411.li'
-if API_URL == 'https://api.t411.ch':
+
+if _API_ == 'https://api.t411.ch':
+    new_url = 'https://api.t411.li'
     provider.ADDON.setSetting("base_url", new_url)
-    API_URL = new_url
+    _API_ = new_url
 
 
 def _init():
-    global user_credentials
+    global USER_CREDENTIALS
     provider.log.info("Get user credentials and authentificate it, "
                       "if any credentials defined use token stored in user file")
     try:
         with open(USER_CREDENTIALS_FILE) as user_cred_file:
-            user_credentials = json.loads(user_cred_file.read())
+            USER_CREDENTIALS = json.loads(user_cred_file.read())
             provider.log.info("Get local credentials")
-            provider.log.debug(user_credentials)
-        if 'uid' not in user_credentials or 'token' not in user_credentials:
+            provider.log.debug(USER_CREDENTIALS)
+        if 'uid' not in USER_CREDENTIALS or 'token' not in USER_CREDENTIALS:
             raise Exception('Wrong data found in user file')
     except IOError:
         # Try to auth user from credentials in parameters
@@ -63,28 +62,28 @@ def _init():
 
 
 def _auth(username, password):
-    global user_credentials
+    global USER_CREDENTIALS
     provider.log.info("Authentificate user and store token")
-    user_credentials = call('/auth', {'username': username, 'password': password})
-    print(user_credentials)
-    if 'error' in user_credentials:
-        raise Exception('Error while fetching authentication token: %s' % user_credentials['error'])
+    USER_CREDENTIALS = call('/auth', {'username': username, 'password': password})
+    print(USER_CREDENTIALS)
+    if 'error' in USER_CREDENTIALS:
+        raise Exception('Error while fetching authentication token: %s' % USER_CREDENTIALS['error'])
     # Create or update user file
     provider.log.info('file %s' % USER_CREDENTIALS_FILE)
-    user_data = json.dumps({'uid': '%s' % user_credentials['uid'], 'token': '%s' % user_credentials['token']})
+    user_data = json.dumps({'uid': '%s' % USER_CREDENTIALS['uid'], 'token': '%s' % USER_CREDENTIALS['token']})
     with open(USER_CREDENTIALS_FILE, 'w') as user_cred_file:
         user_cred_file.write(user_data)
     return True
 
 
 def call(method='', params=None):
-    provider.log.info("Call T411 API: %s%s" % (API_URL, method))
+    provider.log.info("Call T411 API: %s%s" % (_API_, method))
     if method != '/auth':
-        token = user_credentials['token']
+        token = USER_CREDENTIALS['token']
         provider.log.info('token %s' % token)
-        req = provider.POST('%s%s' % (API_URL, method), headers={'Authorization': token})
+        req = provider.POST('%s%s' % (_API_, method), headers={'Authorization': token})
     else:
-        req = provider.POST('%s%s' % (API_URL, method), data=provider.urlencode(params))
+        req = provider.POST('%s%s' % (_API_, method), data=provider.urlencode(params))
     if req.getcode() == 200:
         return req.json()
     else:
@@ -116,7 +115,7 @@ def get_terms(movie=False):
 # Default Search
 def search(query, cat_id=CAT_MOVIE, terms=None, episode=False, season=False):
     provider.notify(message=str(query).replace('+', ' ').title(),
-                    header="Quasar AlexP's [COLOR FF18F6F3]t411[/COLOR] Provider", time=3000, image=icon)
+                    header="Quasar AlexP's [COLOR FF18F6F3]t411[/COLOR] Provider", time=3000, image=_ICON_)
     result = []
     threads = []
     q = Queue.Queue()
@@ -124,12 +123,13 @@ def search(query, cat_id=CAT_MOVIE, terms=None, episode=False, season=False):
     query = query.replace('+', '%20')
     response = call('/torrents/search/%s&?limit=15&cid=%s%s' % (query, cat_id, terms))
     if episode or season:  # search for animation series too
-        response_tv_anim = response = call('/torrents/search/%s&?limit=15&cid=%s%s' % (query, CAT_TV_ANIME, terms))
+        response_tv_anim = response = call('/torrents/search/%s&?limit=15&cid=%s%s' 
+                                           % (query, CAT_SERIES_ANIMATED, terms))
         response['torrents'] = response['torrents'] + response_tv_anim['torrents']
-    if episode and serie_en_plus == 'true':
+    if episode and _FILTER_SERIES_FULL_ == 'true':
         terms2 = terms[:-3] + '936'
         response2 = call('/torrents/search/%s&?limit=15&cid=%s%s' % (query, cat_id, terms2))
-        response3 = call('/torrents/search/%s&?limit=15&cid=%s%s' % (query, CAT_TV_ANIME, terms2))
+        response3 = call('/torrents/search/%s&?limit=15&cid=%s%s' % (query, CAT_SERIES_ANIMATED, terms2))
         response['torrents'] = response['torrents'] + response2['torrents'] + response3['torrents']
     provider.log.debug("Search results : %s" % response)
     # quasar send GET requests & t411 api needs POST
@@ -138,7 +138,7 @@ def search(query, cat_id=CAT_MOVIE, terms=None, episode=False, season=False):
     for t in response['torrents']:
 
         # Call each individual page in parallel
-        thread = Thread(target=torrent2magnet, args=(t, q, user_credentials['token']))
+        thread = Thread(target=torrent2magnet, args=(t, q, USER_CREDENTIALS['token']))
         thread.start()
         threads.append(thread)
 
@@ -156,21 +156,21 @@ def search(query, cat_id=CAT_MOVIE, terms=None, episode=False, season=False):
                        "info_hash": item["info_hash"],
                        "is_private": True,
                        "provider": "[COLOR FF18F6F3]t411[/COLOR]",
-                       "icon": icon})
+                       "icon": _ICON_})
     return result
 
     
 def search_episode(episode):
     terms = ''
-    if filtre_serie == 'true':
+    if _FILTER_SERIES_ == 'true':
         terms = get_terms()
                
     provider.log.debug("Search episode : name %(title)s, season %(season)02d, episode %(episode)02d" % episode)
-    if titreVF == 'true':
+    if _TITLE_VF_ == 'true':
         # Get the FRENCH title from TMDB
         provider.log.debug('Get FRENCH title from TMDB for %s' % episode['imdb_id'])
         response = provider.GET("%s/find/%s?api_key=%s&language=fr&external_source=imdb_id"
-                                % (tmdbUrl, episode['imdb_id'], tmdbKey))
+                                % (TMDB_URL, episode['imdb_id'], TMDB_KEY))
         provider.log.debug(response)
         if response != (None, None):
             episode['title'] = response.json()['tv_results'][0]['name'].encode('utf-8').replace(' ', '+')
@@ -202,21 +202,21 @@ def search_episode(episode):
 
         terms += '&term[46][]=%s' % real_ep
 
-    return search(episode['title'], CAT_TV, terms, episode=True)
+    return search(episode['title'], CAT_SERIES, terms, episode=True)
 
 
 def search_season(serie):
     terms = ''
-    if filtre_serie == 'true':
+    if _FILTER_SERIES_ == 'true':
         terms = get_terms()
 
     terms += '&term[46][]=936'  # saison complete
     
-    if titreVF == 'true':
+    if _TITLE_VF_ == 'true':
         # Get the FRENCH title from TMDB
         provider.log.debug('Get FRENCH title from TMDB for %s' % serie['imdb_id'])
         response = provider.GET("%s/find/%s?api_key=%s&language=fr&external_source=imdb_id"
-                                % (tmdbUrl, serie['imdb_id'], tmdbKey))
+                                % (TMDB_URL, serie['imdb_id'], TMDB_KEY))
         provider.log.debug(response)
         if response != (None, None):
             serie['title'] = response.json()['tv_results'][0]['name'].encode('utf-8').replace(' ', '+')
@@ -236,20 +236,20 @@ def search_season(serie):
     
     terms += '&term[45][]=%s' % real_s
     
-    return search(serie['title'], CAT_TV, terms, season=True)
+    return search(serie['title'], CAT_SERIES, terms, season=True)
     
 
 def search_movie(movie):
     terms = ''
-    if filtre_film == 'true':
+    if _FILTER_MOVIE_ == 'true':
         terms = get_terms(False)
                
-    if titreVF == 'true':
+    if _TITLE_VF_ == 'true':
         # quasar 0.2 doesn't work well with foreing title. Get the FRENCH title from TMDB
         provider.log.debug('Get FRENCH title from TMDB for %s' % movie['imdb_id'])
         response = provider.GET(
             "%s/movie/%s?api_key=%s&language=fr&external_source=imdb_id&append_to_response=alternative_titles"
-            % (tmdbUrl, movie['imdb_id'], tmdbKey)
+            % (TMDB_URL, movie['imdb_id'], TMDB_KEY)
         )
         if response != (None, None):
             response = response.json()
@@ -265,12 +265,12 @@ def search_movie(movie):
 
 def torrent2magnet(t, q, token):
     torrentdl = '/torrents/download/%s' % t["id"]
-    response = provider.POST('%s%s' % (API_URL, torrentdl), headers={'Authorization': token})
+    response = provider.POST('%s%s' % (_API_, torrentdl), headers={'Authorization': token})
     torrent = response.data
-    if passkey is not '':
+    if _PASSKEY_ is not '':
         key = re.compile('download([^"]+)announce').findall(torrent)
         key = key[0].split('/')[1]
-        torrent = torrent.replace(key, passkey)
+        torrent = torrent.replace(key, _PASSKEY_)
     metadata = bencode.bdecode(torrent)
     hashcontents = bencode.bencode(metadata['info'])
     digest = hashlib.sha1(hashcontents).hexdigest()
